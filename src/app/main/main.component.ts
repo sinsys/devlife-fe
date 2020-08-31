3982dd44d1db06cd27283ed7c5f2eccdfe0ffe5e
Nicholas Hazel <nicholas.hazel@gmail.com>
2020-08-30 23:22:00 -0700

    feature: Implemented font awesome icons. Cleaned up terminal

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core'
-import { faCoffee, faCode } from '@fortawesome/free-solid-svg-icons'
+import {
+  faCoffee,
+  faCode
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  faAngular,
+  faReact,
+  faNodeJs,
+  faAws,
+  faGithub,
+  faJsSquare,
+  faLinux,
+  faNpm,
+  faSlack,
+  faStackOverflow
+} from '@fortawesome/free-brands-svg-icons'
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
+
 export class MainComponent {
   faCoffee = faCoffee
   faCode = faCode
+  faAngular = faAngular
+  faReact = faReact
+  faNodeJs = faNodeJs
+  faAws = faAws
+  faGithub = faGithub
+  faJsSquare = faJsSquare
+  faLinux = faLinux
+  faNpm = faNpm
+  faSlack = faSlack
+  faStackOverflow = faStackOverflow
 }
