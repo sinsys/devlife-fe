7306f4a76156828e32672a30c21d016053001d96
Nicholas Hazel <nicholas.hazel@gmail.com>
2020-08-30 22:23:15 -0700

    feature: Automated greeting in terminal

--- src/app/components/terminal/terminal.component.ts
+++ src/app/components/terminal/terminal.component.ts
@@ -1,5 +1,5 @@
 import { Component, AfterViewInit, ViewChild } from '@angular/core'
-import { NgTerminal, FunctionsUsingCSI } from 'ng-terminal'
+import { NgTerminal } from 'ng-terminal'
 
 @Component({
   selector: 'app-terminal',
@@ -11,18 +11,35 @@ export class TerminalComponent implements AfterViewInit {
     'term',
     { static: true }
   ) child!: NgTerminal
-  terminalText = 'Welcome to my world... >>>'
 
-  ngAfterViewInit (): void {
+  terminalText = [
+    'My name is Nicholas Hazel',
+    'I breathe code... and drink coffee',
+    'Welcome to my world... >>>'
+  ]
+
+  writeRows (rows: string[], row = 0): any {
+    if (row >= rows.length) {
+      return true
+    }
+    // Write a row
+    const line = rows[row]
     let i = 0
-    const writeWelcome = setInterval(() => {
-      this.child.write(this.terminalText[i])
+    const timeout = window.setInterval(() => {
+      this.child.write(line[i])
       i++
-      if (i >= this.terminalText.length) {
-        clearInterval(writeWelcome)
+      if (i >= line.length) {
+        clearInterval(timeout)
         this.child.write('\r\n$ ')
+        row++
+        return this.writeRows(rows, row)
       }
     }, 50)
+  }
+
+  ngAfterViewInit (): void {
+    this.writeRows(this.terminalText)
+
     this.child.keyEventInput.subscribe(e => {
       console.log(`keyboard event: ${e.domEvent.keyCode}, ${e.key}`)
 
